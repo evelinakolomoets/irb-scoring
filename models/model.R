e7345a4f7739f54129c3b6bb92018016ab071242
evelinakolomoets <evelina.kolomoets@gmail.com>
2024-06-04 12:11:49 +0300

    upd structure

--- models/model.R
+++ models/model.R
@@ -11,6 +11,15 @@ df <- cbind(df,const = 1)
 data <- pdata.frame(df, index = c("REGN_GKO", "DATE"))
 initial_values <- rep(1, length(names(data)) - 2 -1)  # количество регрессоров
 
+# СОМНИТЕЛЬНО НО ОКЕЙ
+m <- plm(DEFAULT ~ const + MAX_NORM + CAP_TO_NA
+         + RES_TO_NA + NA. + BI_TO_NA + NI_TO_NA
+         + L_NORM + I_NORM + IS_SZKO + REGISTERED_MSC, 
+         data=df)
+summary(m)
+
+stargazer(m, out='out.html')
+
 # Подгонка логистической регрессионной модели с фиксированными эффектами
 model <- pglm(DEFAULT ~ MAX_NORM + CAP_TO_NA + RES_TO_NA + 
                 NA. + 
@@ -30,13 +39,6 @@ glm_logit <- glm(DEFAULT ~ MAX_NORM + CAP_TO_NA + RES_TO_NA + NA. + BI_TO_NA + N
              data = data,
              family = binomial("logit"))
 summary(glm_logit)
-
-# СОМНИТЕЛЬНО НО ОКЕЙ
-m <- plm(DEFAULT ~ const+MAX_NORM+CAP_TO_NA
-             +RES_TO_NA+NA.+BI_TO_NA+NI_TO_NA
-             +L_NORM+I_NORM+IS_SZKO+REGISTERED_MSC, 
-             data=df)
-summary(m)
 
 
 # ХРЕНЬ
